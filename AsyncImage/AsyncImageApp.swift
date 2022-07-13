a700bd966f1ff408012b10b4058c43b42fe0ff0f
benzkittisak <benzkittisak@stv.ac.th>
2022-07-13 20:57:34 +0700

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Kittisak Panluea on 13/7/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncImage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AsyncImage
+//
+//  Created by Kittisak Panluea on 13/7/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
